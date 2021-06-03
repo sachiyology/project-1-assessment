5bc0093b61ebd4a9e1601accba7098cbc4d99258
sachiyo sugaya <sachiyology@gmail.com>
2021-06-03 11:06:27 +0900

    update

--- js/main.js
+++ js/main.js
@@ -10,3 +10,22 @@ window.addEventListener('DOMContentLoaded', ()=>{
     counter.textContent=parseInt(counter.textContent)+parseInt(text.value);;
   });
 });
+
+
+window.addEventListener('load',()=>{  // index.html読込時
+
+    const elems = document.querySelectorAll('[hovercolor]');  // hovercolorという属性を持つ要素すべて
+
+    elems.forEach((e)=>{  // 上記で指定した要素それぞれ(=e)について
+
+        e.addEventListener('mouseover',()=>{  // ホバー時
+            e.style.color = e.getAttribute('hovercolor');
+            // eのテキスト色 ＝ eのhovercolorの属性値
+        });
+
+        e.addEventListener('mouseout',()=>{  // ホバー解除時
+            e.style.color = "";  // 初期値に戻す（CSSで指定したもの）
+        });
+
+    });
+});
